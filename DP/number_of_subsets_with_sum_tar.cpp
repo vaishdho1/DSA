7ccd6a4051a3148073b011340e3429761bcab41f
Vaishnavi Panchavati <38342947+vaishdho1@users.noreply.github.com>
2022-10-10 21:24:28 +0530

    Create number_of_subsets_with_sum_tar.cpp

---
+++ DP/number_of_subsets_with_sum_tar.cpp
@@ -0,0 +1,32 @@
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here.
+    int n = num.size();
+    vector<int>prev(tar+1,0);
+    
+    
+    if(num[0]<=tar)
+        prev[num[0]] = 1;
+    if(num[0]==0)
+        prev[0] = 2;
+    else
+        prev[0] = 1;
+    
+    for(int i=1;i<n;i++)
+    {
+        vector<int>cur(tar+1,0);
+        for(int k=0;k<=tar;k++)
+        {
+            if(num[i]<=k)
+            { 
+               cur[k]+=prev[k-num[i]];
+            }
+            cur[k] += prev[k];
+            
+        }
+
+        prev = cur;
+    }
+    return prev[tar];
+   
+}
