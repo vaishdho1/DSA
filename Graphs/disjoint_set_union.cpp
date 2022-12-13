0e70725655241615b3274e19c64c49acdb62d9c7
Vaishnavi Panchavati <38342947+vaishdho1@users.noreply.github.com>
2022-12-13 15:55:10 +0530

    Update disjoint_set_union.cpp

--- Graphs/disjoint_set_union.cpp
+++ Graphs/disjoint_set_union.cpp
@@ -1,3 +1,10 @@
+/*
+Implemented disjoint set data structure both union by rank and union by size
+part.
+TC:O(4alpha) ~O(1)
+*/
+
+
 #include <iostream>
 #include<string>
 #include<vector>
@@ -6,12 +13,13 @@ using namespace std;
 
 class Disjoint_set
 {
-    vector<int>rank,parent;
+    vector<int>rank,parent,size;
     public:
     Disjoint_set(int n)
     {
         rank.resize(n+1,0);
         parent.resize(n+1);
+        size.resize(n+1,1);
         for(int i=0;i<=n;i++)
             parent[i] = i;
     }
@@ -22,6 +30,25 @@ class Disjoint_set
         if(node == parent[node])
             return node;
         return parent[node] = findpar(parent[node]);
+    }
+
+    void unionBysize(int u,int v)
+    {
+        int par_u = findpar(u);
+        int par_v = findpar(v);
+
+        if(par_u==par_v)
+            return;
+        if(size[par_u] > size[par_v])
+        {
+            parent[par_v] = par_u;
+            size[par_u] += size[par_v];
+        }
+        else
+        {
+            parent[par_u] = par_v;
+            size[par_v] += size[par_u];
+        }
     }
 
     void unionByRank(int u,int v)
@@ -47,16 +74,16 @@ class Disjoint_set
 int main()
 {
     Disjoint_set ds(7);
-    ds.unionByRank(1,2);
-    ds.unionByRank(2,3);
-    ds.unionByRank(4,5);
-    ds.unionByRank(6,7);
-    ds.unionByRank(5,6);
+    ds.unionBysize(1,2);
+    ds.unionBysize(2,3);
+    ds.unionBysize(4,5);
+    ds.unionBysize(6,7);
+    ds.unionBysize(5,6);
     if(ds.findpar(3) == ds.findpar(7))
      cout<<"same"<<endl;
     else
         cout<<"not same"<<endl;
-      ds.unionByRank(3,7);
+      ds.unionBysize(3,7);
        if(ds.findpar(3) == ds.findpar(7))
      cout<<"same"<<endl;
     else
