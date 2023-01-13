114ce5e476f2b09311588aac2cd45b4739ae4e78
Vaishnavi Panchavati <38342947+vaishdho1@users.noreply.github.com>
2023-01-13 22:03:35 +0530

    Update Number of Ways to Separate Numbers.cpp

--- DP/Number of Ways to Separate Numbers.cpp
+++ DP/Number of Ways to Separate Numbers.cpp
@@ -1,9 +1,12 @@
 /*
+dp with prefix sum optimization
 You wrote down many positive integers in a string called num. 
 However, you realized that you forgot to add commas to seperate the different numbers.
 You remember that the list of integers was non-decreasing and that no integer had leading zeros.
 Return the number of possible lists of integers that you could have written down to get the string num.
 Since the answer may be large, return it modulo 109 + 7.
+Approach:
+Normal dp with prefix sum for optimization.
 */
 
 class Solution {
